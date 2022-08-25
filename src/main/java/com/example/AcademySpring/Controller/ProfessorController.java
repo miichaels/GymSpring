0f8e6fb1a43ab7c435d75ac81a3c66e29a7b54be
miichaels <michael_araujo2012@hotmail.com>
2022-08-25 16:10:38 -0300

    CRUD completo de Professor - teste com Insomnia ok

--- src/main/java/com/example/AcademySpring/Controller/ProfessorController.java
+++ src/main/java/com/example/AcademySpring/Controller/ProfessorController.java
@@ -1,11 +1,44 @@
 package com.example.AcademySpring.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.AcademySpring.Model.ProfessorModel;
+import com.example.AcademySpring.Repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/professor")
 @CrossOrigin
 public class ProfessorController {
+
+    @Autowired
+    public ProfessorRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<ProfessorModel>> GetAll(){
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfessorModel> postProf (@RequestBody @Valid ProfessorModel professorModel) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(professorModel));
+    }
+
+    @PutMapping
+    public ResponseEntity<ProfessorModel> putProf (@RequestBody @Valid ProfessorModel professorModel){
+        return ResponseEntity.ok(repository.save(professorModel));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete (@PathVariable Long id){
+        return repository.findById(id).map(resposta -> {repository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();})
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
